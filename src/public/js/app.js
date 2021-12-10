8fbd87f4a6219adda76732f5ad6e7463230bf28c
Tetuani488 <solaymanv@gmail.com>
2021-12-10 02:28:51 +0100

    updated 15

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -42,6 +42,61 @@ $(document).ready(function(){
         const postId = $(this).data("id");
         window.location = ('/posts/'+postId) 
     })
+    
+    $(".post__media-wrapper").each(function () {
+        let subwrarpper1 = [];
+        let subwrarpper2 = [];
+        let length = $(this).data("lentgh")
+        $(this).find(".post__media-thumbnail-container").each(function (index) {
+            if(length ==2){
+                if (index === 0){
+                    subwrarpper1.push({
+                        filename: $(this).attr("value"),
+                        pId: $(this).data("ajax")
+                    })
+                }else{
+                    subwrarpper2.push({
+                        filename: $(this).attr("value"),
+                        pId: $(this).data("ajax")
+                    })
+                }
+            }else if(length >2){
+                if (index <=1){
+                    subwrarpper1.push({
+                        filename: $(this).attr("value"),
+                        pId: $(this).data("ajax")
+                    })
+                }else{
+                    subwrarpper2.push({
+                        filename: $(this).attr("value"),
+                        pId: $(this).data("ajax")
+                    })
+                }
+            }
+        });
+        $(this).html("")
+        if (subwrarpper1.length >0){
+            $(this).append('<div class="post__media-subwrapper" id="subwrarpper1"></div>');
+            subwrarpper1.forEach(element => {
+                $(this).find("#subwrarpper1").append('\
+                <div class="post__media-thumbnail-container" data-ajax="'+element.pId+'" value="'+element.filename+'">\
+                    <img src="/public/upload/media/images/'+element.filename+'" alt="" class="post__media-image">\
+                </div>\
+                ');
+            });
+        }
+        console.log($(this).html())
+        if (subwrarpper2.length >0){
+            $(this).append('<div class="post__media-subwrapper" id="subwrarpper2"></div>');
+            subwrarpper2.forEach(element => {
+                $(this).find("#subwrarpper2").append('\
+                <div class="post__media-thumbnail-container" data-ajax="'+element.pId+'" value="'+element.filename+'">\
+                    <img src="/public/upload/media/images/'+element.filename+'" alt="" class="post__media-image">\
+                </div>\
+                ');
+            });
+        }
+    });
 
     //Open image modal
     $('.post__media-thumbnail-container').click(function (event) {
@@ -55,5 +110,10 @@ $(document).ready(function(){
     $('.modal__button-cross').click(function () {
         const modal = $(this).attr("value");
         $(modal).hide();
+    });
+
+    $('.modal__media').click(function (event) {
+        const modal = $(this).attr("value");
+        $(".modal__media").hide();
     });
 })
